c80353429d86cb014db034ed14f4936432fe4267
Juan D. Collada Vasquez <cvjuan270@gmail.com>
2025-02-28 07:42:04 -0500

    fix styles

--- certificate_gexin/models/survey_user_input.py
+++ certificate_gexin/models/survey_user_input.py
@@ -7,7 +7,10 @@ class SurveyUserInput(models.Model):
 
     def get_formatted_duration(self):
         if self.slide_id:
-            hours = int(self.slide_id.completion_time)
-            minutes = int((self.slide_id.completion_time - hours) * 60)
-            return f"{hours}:{minutes:02d}"
+            if self.slide_id.completion_time > 0:
+                hours = int(self.slide_id.completion_time)
+                # minutes = int((self.slide_id.completion_time - hours) * 60)
+                # return f"{hours}:{minutes:02d}"
+                return hours
+            return False
         return False
